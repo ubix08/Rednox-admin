5c72fa29d3ed0388a5c28119d2d0e347017d78a7
ubix08 <98852326+ubix08@users.noreply.github.com>
2026-01-07 19:17:26 +0100

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="bg-gray-800 text-white p-4 flex justify-between">
+    <h1 class="text-xl">RedNox Admin</h1>
+    <nav>
+      <RouterLink to="/" class="mx-2">Flows</RouterLink>
+      <RouterLink to="/stats" class="mx-2">Stats</RouterLink>
+    </nav>
+  </header>
+  <main class="p-4">
+    <RouterView />
+  </main>
+</template>
+
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router';
+</script>
